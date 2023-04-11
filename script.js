a1db0bcf9f623cde84ee66771f5f0ef226098fd8
M1zir <abubakra7mad@gmail.com>
2023-04-11 15:53:15 +0400

    final website

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ window.addEventListener('DOMContentLoaded', (event) => {
       path: 'animation.json'
     });
   });
+
+  var target_mili_sec = new Date("Dec 10, 2023 14:30:0").getTime();
+  function timer() {
+      var now_mili_sec = new Date().getTime();
+      var remaining_sec = Math.floor( (target_mili_sec - now_mili_sec) / 1000 );
+
+      var day = Math.floor(remaining_sec / (3600 * 24));
+      var hour = Math.floor((remaining_sec % (3600 * 24)) / 3600);
+      var min = Math.floor((remaining_sec % 3600) / 60);
+      var sec = Math.floor(remaining_sec % 60);
+
+      document.querySelector("#day").innerHTML = day;
+      document.querySelector("#hour").innerHTML = hour;
+      document.querySelector("#min").innerHTML = min;
+      document.querySelector("#sec").innerHTML = sec;
+  }
+
+  setInterval(timer, 1000); //1000 it means 1 sec
